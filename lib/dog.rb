def05e5bceb2adf9b5e0b36787099e6fa7b9867b
korey ko <koreykendrick1@gmail.com>
2020-06-04 22:02:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,14 @@
 class Dog 
   @@all =[]
   
+  attr_accessor :new_dog
   
   def initialize=(new_dog)
     @new_dog = new_dog
     @@all << self 
   end 
-  
+   
+   def self 
+     @@self.print_all 
+   end 
+   
