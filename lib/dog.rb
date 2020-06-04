017d88665af975a3af5fcc87d98bbb1eb6b7300b
korey ko <koreykendrick1@gmail.com>
2020-06-04 22:14:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,12 +14,15 @@ class Dog
      @@all =[] 
    end 
    
-   def 
+   def self.print_all 
      @@all.each do |dog|
        puts dog.name 
      end 
-   end 
-   def
-     
-   end 
  end
+ def save 
+   @@all << self 
+ end 
+ def name 
+   @nameend
+ end 
+ 
